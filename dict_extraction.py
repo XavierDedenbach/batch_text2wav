49b9a249419a1a8883e0e793bd14ada9e19deaa3
XavierDedenbach <xdedenbach6@gmail.com>
2024-09-17 11:23:41 -0700

    Update dict_extraction to create a xml type .pls file to capture the relationship between english and IPA pronounciation

--- dict_extraction.py
+++ dict_extraction.py
@@ -65,10 +65,53 @@ class Extractor:
                 # If the line has at least one part, write the first part to the output file
                 if len(parts) > 0:
                     f.write(parts[0].strip() + '\n')
+   
+    def extract_lexemes(self):
+        """
+        Extracts English words and their IPA spellings from the input file and writes them to the output file in the desired format.
+
+        Note:
+        ----
+        This method assumes that the input file is a tab-separated file where the first column contains the English words and the second column contains the IPA spellings.
+        """
+        # Detect the encoding of the input file
+        encoding = self.detect_encoding()
+        # Open the input file in read mode with the detected encoding
+        with open(self.input_file, 'r', encoding=encoding, errors='ignore') as f:
+            # Read all lines from the input file
+            lines = f.readlines()
+        # Open the output file in write mode with UTF-8 encoding
+        with open(self.output_file, 'w', encoding='utf-8') as f:
+            # Write the header to the output file
+            f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+            f.write('<lexicon version="1.0"\n')
+            f.write('      xmlns="http://www.w3.org/2005/01/pronunciation-lexicon"\n')
+            f.write('      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n')
+            f.write('      xsi:schemaLocation="http://www.w3.org/2005/01/pronunciation-lexicon\n')
+            f.write('        http://www.w3.org/TR/2007/CR-pronunciation-lexicon-20071212/pls.xsd"\n')
+            f.write('      alphabet="ipa" xml:lang="en-US">\n')
+            
+            # Iterate over each line in the input file
+            for line in lines:
+                # Split the line into parts using the tab character as the delimiter
+                parts = line.split('\t')
+                # If the line has at least two parts, write the lexeme to the output file
+                if len(parts) > 1:
+                    english_word = parts[0].strip()
+                    ipa_spelling = parts[1].strip()
+                    f.write('  <lexeme>\n')
+                    f.write('    <grapheme>{}</grapheme>\n'.format(english_word))
+                    f.write('    <phoneme>{}</phoneme>\n'.format(ipa_spelling))
+                    f.write('  </lexeme>\n')
+            
+            # Close the lexicon tag
+            f.write('</lexicon>\n')
 
     def run(self):
-        self.extract_english_words()
+        self.extract_lexemes() 
+        # elf.extract_english_words()
 
 if __name__ == '__main__':
-    extractor = Extractor('dict.tsv', 'just_english.txt')
+    extractor = Extractor('dict.tsv', 'dictionary.pls') #Lexemes format
+    # extractor = Extractor('dict.tsv', 'just_english.txt') #English words format
     extractor.run()
